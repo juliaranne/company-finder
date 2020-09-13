c74f15bbf34ed77156b96709a3d47714721fc9e2
Julia Robinson <juliaarobinson@hotmail.com>
2020-09-13 14:32:44 +0100

    get json data

--- src/CompanyFinder.js
+++ src/CompanyFinder.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 import './styles/company_finder.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+class CompanyFinder extends React.Component {
+  componentDidMount() {
+    axios.get('http://localhost:5000/Customers')
+      .then((response) => console.log('This is your data', response.data))
+      .catch((error) => console.log(error));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+      </div>
+    )
+  };
 }
 
-export default App;
+export default CompanyFinder;
