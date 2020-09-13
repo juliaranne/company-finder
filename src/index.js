caa48236e9d4f3dfb011c2ad34dde39f39b836f6
Julia Robinson <juliaarobinson@hotmail.com>
2020-09-13 13:47:58 +0100

    convert to use scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import CompanyFinder from './CompanyFinder';
 import * as serviceWorker from './serviceWorker';
 
