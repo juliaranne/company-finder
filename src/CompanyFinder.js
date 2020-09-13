f28a9a27cc77ba034a180b84c9983722ab139422
Julia Robinson <juliaarobinson@hotmail.com>
2020-09-13 14:54:26 +0100

    map response to get countries

--- src/CompanyFinder.js
+++ src/CompanyFinder.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import axios from 'axios';
 import './styles/company_finder.scss';
+import SelectList from './components/SelectList';
 
 class CompanyFinder extends React.Component {
+  state = {
+    customers: []
+  }
+
   componentDidMount() {
     axios.get('http://localhost:5000/Customers')
-      .then((response) => console.log('This is your data', response.data))
+      .then((response) => this.setState({customers: response.data}))
       .catch((error) => console.log(error));
+  }
+
+  getCountries = () => {
+    const customerData = this.state.customers.map(item => item.Country);
+    console.log(customerData);
+    return customerData;
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <SelectList countries={this.getCountries()} />  
         </header>
       </div>
     )
---
+++ src/components/SelectList.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SelectList = (props) => <h1>hello</h1>
+
+export default SelectList
